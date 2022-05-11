a053de5861db219e8a18cf1d6c4758d69b11e629
wowotou <2269030597@qq.com>
2022-05-11 09:01:21 +0800

    2022 05 10

--- Models/CIFAR10.py
+++ Models/CIFAR10.py
@@ -63,28 +63,39 @@ class BasicBlock(nn.Module):
         super(BasicBlock, self).__init__()
         self.bn1 = nn.BatchNorm2d(in_planes)
         self.relu1 = nn.LeakyReLU(0.1, inplace=True)
-        self.conv1 = nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride,
-                               padding=1, bias=False)
+        self.conv1 = nn.Conv2d(in_planes,
+                               out_planes,
+                               kernel_size=3, stride=stride, padding=1,
+                               bias=False)
+
         self.bn2 = nn.BatchNorm2d(out_planes)
         self.relu2 = nn.LeakyReLU(0.1, inplace=True)
-        self.conv2 = nn.Conv2d(out_planes, out_planes, kernel_size=3, stride=1,
-                               padding=1, bias=False)
+        self.conv2 = nn.Conv2d(out_planes,
+                               out_planes,
+                               kernel_size=3, stride=1, padding=1,
+                               bias=False)
+
         self.droprate = dropRate
-        self.equalInOut = (in_planes == out_planes)
-        self.convShortcut = (not self.equalInOut) and nn.Conv2d(in_planes, out_planes, kernel_size=1, stride=stride,
-                                                                padding=0, bias=False) or None
+
+        self.In_equal_Out = (in_planes == out_planes)
+        self.convShortcut = (not self.In_equal_Out) and nn.Conv2d(in_planes,
+                                                                out_planes,
+                                                                kernel_size=1, stride=stride, padding=0,
+                                                                bias=False) or None
 
     def forward(self, x):
-        if not self.equalInOut:
+        if not self.In_equal_Out:
             x = self.relu1(self.bn1(x))
         else:
             out = self.relu1(self.bn1(x))
-        out = self.relu2(self.bn2(self.conv1(out if self.equalInOut else x)))
+
+        out = self.relu2(self.bn2(self.conv1(out if self.In_equal_Out else x)))
+
         if self.droprate > 0:
             out = F.dropout(out, p=self.droprate, training=self.training)
         out = self.conv2(out)
         # 这个torch.add 很重要
-        return torch.add(x if self.equalInOut else self.convShortcut(x), out)
+        return torch.add(x if self.In_equal_Out else self.convShortcut(x), out)
 
 
 class NetworkBlock(nn.Module):
@@ -95,7 +106,10 @@ class NetworkBlock(nn.Module):
     def _make_layer(self, block, in_planes, out_planes, nb_layers, stride, dropRate):
         layers = []
         for i in range(int(nb_layers)):
-            layers.append(block(i == 0 and in_planes or out_planes, out_planes, i == 0 and stride or 1, dropRate))
+            layers.append(block(i == 0 and in_planes or out_planes,
+                                out_planes,
+                                i == 0 and stride or 1,
+                                dropRate))
         return nn.Sequential(*layers)
 
     def forward(self, x):
