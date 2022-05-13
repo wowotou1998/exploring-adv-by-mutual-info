f05193f919f3f9e2d03f4ad9eba765aa3ee94747
zjb <2269030597@qq.com>
2022-05-13 10:43:22 +0800

    2022 05 13

--- MI_flow_in_training_newest.py
+++ MI_flow_in_training_newest.py
@@ -332,16 +332,16 @@ if __name__ == '__main__':
     import argparse
 
     # Model, Model_Name = FC_2(Activation_F=nn.ReLU()), 'FC_2'
-    # Model, Model_Name = LeNet_cifar10(), 'LeNet_cifar10'
+    Model, Model_Name = LeNet_cifar10(), 'LeNet_cifar10'
     # Model, Model_Name = net_cifar10(), 'net_cifar10'
     # Model, Model_Name = VGG_s(), 'VGG_s_11'
     # Model, Model_Name = resnet18(pretrained=False, num_classes=10), 'resnet18'
     # Model, Model_Name = resnet34(pretrained=False, num_classes=10), 'resnet34'
     # Model, Model_Name = vgg11(pretrained=False)
-    Model, Model_Name = WideResNet(depth=1 * 6 + 4, num_classes=10, widen_factor=1, dropRate=0.0), 'WideResNet'
+    # Model, Model_Name = WideResNet(depth=1 * 6 + 4, num_classes=10, widen_factor=1, dropRate=0.0), 'WideResNet'
 
     parser = argparse.ArgumentParser(description='training arguments with PyTorch')
-    parser.add_argument('--Std_Epoch_Num', default=100, type=int, help='The epochs.')
+    parser.add_argument('--Std_Epoch_Num', default=150, type=int, help='The epochs.')
     parser.add_argument('--Learning_Rate', default=1e-1, type=float, help='The learning rate.')
     parser.add_argument('--Forward_Size', default=1000, type=int, help='Forward_Size.')
     parser.add_argument('--Forward_Repeat', default=5, type=bool, help='Forward_Repeat')
