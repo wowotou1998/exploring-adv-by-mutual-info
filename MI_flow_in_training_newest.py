9fdaa2154564aaef30d6292df76420794e06b99d
zjb <2269030597@qq.com>
2022-05-14 19:57:42 +0800

    2022 05 14

--- MI_flow_in_training_newest.py
+++ MI_flow_in_training_newest.py
@@ -347,7 +347,7 @@ if __name__ == '__main__':
 
     parser = argparse.ArgumentParser(description='Training arguments with PyTorch')
     parser.add_argument('--Std_Epoch_Num', default=100, type=int, help='The epochs.')
-    parser.add_argument('--Learning_Rate', default=1e-2, type=float, help='The learning rate.')
+    parser.add_argument('--Learning_Rate', default=1e-1, type=float, help='The learning rate.')
     parser.add_argument('--Forward_Size', default=1000, type=int, help='Forward_Size.')
     parser.add_argument('--Forward_Repeat', default=5, type=bool, help='Forward_Repeat')
     parser.add_argument('--GPU', default=0, type=int, help='The GPU id.')
